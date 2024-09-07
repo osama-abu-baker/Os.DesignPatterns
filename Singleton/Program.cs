efb3cb2a5b58484a3aab7bd9f3cffe58d18b0d65
Osama Abu Baker <osamaabubaker111@gmail.com>
2024-09-07 06:19:32 -0400

    Add project files.

---
+++ Singleton/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Singleton
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var databaseConnection = DatabaseConnection.Instance;
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    public class DatabaseConnection
+    {
+        private static DatabaseConnection _instance;
+        private static readonly object _lock = new();
+
+        private DatabaseConnection() { }
+
+        public static DatabaseConnection Instance
+        {
+            get
+            {
+                lock (_lock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new DatabaseConnection();
+                    }
+
+                    return _instance;
+                }
+            }
+        }
+    }
+}
